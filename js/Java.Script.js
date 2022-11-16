e9d8804fdaaa340a0fbdf2af8c677d856194e807
Iuli4 <yulya.popa.962005@gmail.com>
2022-11-16 20:50:29 +0200

    Sladerssss

--- js/Java.Script.js
+++ js/Java.Script.js
@@ -40,13 +40,44 @@ btnSrc.forEach(element => {
     shortLine.className = "text_decoration short-line";
 
     const longLine1 = document.createElement("div")
-    longLine1.className ="text_decoration long-line";
+    longLine1.className ="text_decoration long-line";  //создаем класс
 
     const footerContBar = document.createElement("div")
     footerContBar.className = "footer_cont_bar";
     
     divContainer.append(img, longLine, shortLine, longLine1, footerContBar)
     slideContainer.append(divContainer)
+  })
+
+  const buttons = document.querySelectorAll(".btn");
+
+  const cardsStyle = [
+    {position: "600px", color: "#EFADC3FF"},
+    {position: "0", color: "#9392E2FF"},
+    {position: "-800px", color: "#EDE34DFF"},
+  ]
+
+//arraw function тоже самое что и function
+  const initialPosition = () => {
+    buttons[0].classList.add('active'); //добавляем в уже существующий класс
+    body.style.backgroundColor = cardsStyle[0].color;
+    slideContainer.style.marginLeft = cardsStyle [0].position;
   }
 
-  )
+  const slideshow = () => {
+    initialPosition()
+    buttons.forEach ((btn,idx) => {
+      btn.addEventListener('click', () => {   //следит за картинками и ожидает событие которое будет после клика
+        buttons.forEach(el => el.classList.remove('active'));
+        btn.classList.add('active');
+        body.style.backgroundColor = cardsStyle[idx].color;
+        slideContainer.style.marginLeft = cardsStyle[idx].position;
+      });
+    });
+  };
+
+  slideshow()
+
+
+
+  
